423797d2c7ec8829c3a7dac67ba13d66a9fc6a3f
Julian362 <Juliangarcia362@hotmail.com>
2023-10-13 16:45:24 -0500

    dependencias

--- src/assets/env.js
+++ src/assets/env.js
@@ -3,12 +3,12 @@
 
   // API url
 
-  window._env.HOST_3000 = "172.31.22.171:30000";
-  window._env.HOST_3001 = "172.31.22.171:3000";
-  window._env.HOST_3002 = "172.31.22.171:30002";
-  window._env.HOST_3002 = "172.17.187.239:300002";
-  window._env.HOST_3003 = "172.31.22.171:30003";
-  window._env.HOST_81 = "172.31.22.171:81";
+  window._env.HOST_3000 = "172.23.246.88:30000";
+  window._env.HOST_3001 = "172.23.246.88:30001";
+  window._env.HOST_3002 = "172.23.246.88:30002";
+  window._env.HOST_3002 = "172.23.246.88:30002";
+  window._env.HOST_3003 = "172.23.246.88:30003";
+  window._env.HOST_81 = "172.23.246.88:30081";
   // window._env.HOST_3000 = "localhost:3000";
   // window._env.HOST_3001 = "localhost:3001";
   // window._env.HOST_3002 = "localhost:3002";
